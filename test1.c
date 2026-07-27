8c67346bdc83128caf7e866e881777e4e05b4bf0
RAHUL REDDY <rahulreddy@RAHULs-MacBook-Air.local>
2026-07-28 01:14:05 +0530

    Remove duplicate test C files

--- test1.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello World")  // Missing semicolon
-    return 0;
-}  // Missing semicolon
--- test3.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("%d %d", 5);  // printf expects 3 args, got 2
-    return 0;
-}
